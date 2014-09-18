34f8d60b6f52147497903afc193cf254917b434e
juliocm18 <julioc_m18@hotmail.com>
2014-09-18 10:34:43 -0500

    primera subida demo
    ya estaba terminado

---
+++ queryloader/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor: "#49B6E3",
+        backgroundColor: "#fff",
+        percentage: true,
+        barHeight: 2,
+        completeAnimation: "grow",
+        minimumTime: 500
+    });
+});
